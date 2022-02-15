7db6ab5bdec28c46d9309aec88da5405ab7839cb
unhappyJavaMan <lemel1993@gmail.com>
2022-02-15 18:51:01 +0800

    debug

--- CFA104G1-test/src/main/java/com/roomType/controller/RoomTypeServlet.java
+++ CFA104G1-test/src/main/java/com/roomType/controller/RoomTypeServlet.java
@@ -15,7 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-import com.mysql.cj.jdbc.Blob;
 import com.room.model.RoomService;
 import com.room.model.RoomVO;
 import com.roomType.model.*;
